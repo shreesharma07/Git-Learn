62d77ed7729dd57a0e9e0ea3b55350070db6c756
Shree Kumar Sharma <54742998+shreesharma07@users.noreply.github.com>
2021-07-27 16:28:23 +0530

    `Delete Step.js`

--- Steps/Step.js
+++
@@ -1,9 +0,0 @@
-// --> Printing Line onto Console 
-// Importing Module
-const chalk = require('chalk')
-// Function for Printing String //
-const call = (str) => {
-    console.log('String Passed :: '+chalk.magenta.bold(str))
-}
-let string = 'This is a simple Log Text File'
-call(string)// Function Calling 
